003d8a9e745fe8f2d69754c0c7d1d4d37ab108d8
adalessa <dalessandro.ariel@gmal.com>
2023-01-25 22:11:08 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Home Manager configuration of Jane Doe";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixgl.url = "github:guibou/nixGL";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    home-manager,
+    nixgl,
+    ...
+  }: let
+    system = "x86_64-linux";
+    # pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ nixgl.overlay ];
+    };
+  in {
+    homeConfigurations.alpha = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [./home-alpha.nix];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+    };
+    homeConfigurations.work = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [./home-work.nix];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+    };
+  };
+}
---
+++ packages.nix
@@ -0,0 +1,26 @@
+{config, pkgs, lib, ...}: {
+  home.packages = [
+    pkgs.lazygit
+    pkgs.gpick
+    pkgs.insomnia
+    pkgs.lsd
+    pkgs.php82
+    pkgs.php82Packages.composer
+    pkgs.php82Packages.phpstan
+    pkgs.php82Packages.phpcs
+    pkgs.go
+    pkgs.rustup
+    pkgs.nodejs
+    pkgs.nodePackages.npm
+    pkgs.bat
+    pkgs.jq
+    pkgs.ripgrep
+    pkgs.tmux
+    pkgs.screenkey
+    #formatter for nix
+    pkgs.alejandra
+    # zsh needs
+    pkgs.zsh-syntax-highlighting
+    pkgs.nix-zsh-completions
+  ];
+}
